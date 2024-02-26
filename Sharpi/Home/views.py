e914281389c4084309e8467d6f45c3a20748cb9d
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-02-26 12:00:03 -0500

    last commit befor init 0

--- Sharpi/Home/views.py
+++ Sharpi/Home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_home_section(request):
-    return render(request, 'Home/index.html')
+    return render(request, 'index.html')
