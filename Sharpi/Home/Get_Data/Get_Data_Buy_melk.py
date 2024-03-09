8f1160583446c3fd04570070b94563f72ec93464
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-03-09 09:53:55 -0500

    creat a application in sharpi home for get data from divar

---
+++ Sharpi/Home/Get_Data/Get_Data_Buy_melk.py
@@ -0,0 +1,14 @@
+# Import the requests library
+import requests
+from bs4 import BeautifulSoup
+import mysql.connector
+import json
+import datetime
+
+class Buy_melk_New_data():
+    def __init__(self):
+        pass
+    def New_Melk(self):
+        pass
+
+    
