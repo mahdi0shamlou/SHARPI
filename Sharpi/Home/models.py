0a640274eecc3e04328d46ee48c6bd46bde56db8
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-03-11 12:36:37 -0400

    last commit befor init 0 and it was making two model for django model and it work and very nice

--- Sharpi/Home/models.py
+++ Sharpi/Home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Tokens_alredy_have(models.Model):
+  id = models.IntegerField(primary_key=True, auto_created=True)
+  token = models.CharField(max_length=20)
+  is_details = models.IntegerField()
+  class Meta:
+    db_table = 'Tokens_alredy_have'
+class Aparteman_buy_details(models.Model):
+  id = models.IntegerField(primary_key=True, auto_created=True)
+  token = models.CharField(max_length=20)
+  mahal_english = models.CharField(max_length=255)
+  class Meta:
+    db_table = 'Aparteman_buy_details'
 # Create your models here.
--- Sharpi/Home/views.py
+++ Sharpi/Home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Tokens_alredy_have, Aparteman_buy_details
 
 
 # Create your views here.
 def index_home_section(request):
+    my_list = Tokens_alredy_have.objects.all().values()
+    my_list1 = Aparteman_buy_details.objects.all().values()
+    print(my_list)
+    print(my_list1)
     return render(request, 'index.html')
